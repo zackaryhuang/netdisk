5230d9b552bd4a835ce3cf710e630d915be6ff46
Zackary Huang <huangchao0623@126.com>
2023-08-29 23:08:00 +0800

    Initial Commit

---
+++ netdisk/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  netdisk
+//
+//  Created by Zackary on 2023/8/29.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ netdisk/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  netdisk
+//
+//  Created by Zackary on 2023/8/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
